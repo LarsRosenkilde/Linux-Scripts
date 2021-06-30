2210bd1ed017e3b7fbf702acdb12ce01e7cd146f
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-06-30 17:17:08 +0200

    major

--- ssh_generator.py
+++ ssh_generator.py
@@ -1,24 +1,64 @@
-#/usr/bin/python3
-
 import os
-#import hashlib
+import random
+
+class SSH_Setup(object):
+    	
+ssh_location = "/etc/ssh"
+
 
 def ssh_installer():
 	os.system("sudo apt install openssh-server")
 	os.system("cls")
 
-def save_old_keys():
-	os.mkdir("/etc/ssh/old_keys")
-	os.system("mv ssh_h* old_keys")
 
-def generate_new_keys():
+def key_manager():
+	os.mkdir(f"{ssh_location}/old_keys")
+	os.system("mv ssh_h* old_keys")
 	os.chdir("/etc/ssh/")
 	os.system("dpkg-reconfigure openssh-server")
+
 
 def key_printer():
 	os.system("md5sum /etc/ssh/old_keys/ssh_h*")
 	print("-" * 70)
 	os.system("md5sum /etc/ssh/ssh_h*")
+
+
+def port_changer():
+    new_port = random.randrange(1024, 32767)
+	sshd_file = open(file_path, "r")
+	list_of_lines = sshd_file.readlines()
+	list_of_lines[14] = f"Port {new_port}\n"
+	
+	sshd_file = open(file_path, "w")
+	sshd_file.writelines(list_of_lines)
+	sshd_file.close()
+	return print(f"New SSH port set to {new_port}")
+
+
+if __name__ == "__main__":
+	while True:
+		try:
+			if os.path.isdir(ssh_location):
+    			os.chdir(ssh_location)
+				if os.path.isdir("/etc/ssh/old_keys"):
+					#key_validator()
+					key_printer()
+					break
+				else:
+					key_manager()
+					continue
+			else:
+				ssh_installer()
+				continue
+		except:
+			print("Something went wrong... This script is for debian based distros..\n\n")
+			print("Checking placement of ssh keys...\n")
+			os.system("locate ssh_host_rsa_key")
+			break
+
+
+
 
 """
 def key_validator():
@@ -39,24 +79,3 @@ def key_validator():
 	for index, key in enumerate(old_keys):
 		print(key, new_keys[index])
 """
-
-if __name__ == "__main__":
-	while True:
-		try:
-			if os.path.isdir("/etc/ssh"):
-				if os.path.isdir("/etc/ssh/old_keys"):
-					#key_validator()
-					key_printer()
-					break
-				else:
-					save_old_keys()
-					generate_new_keys()
-					continue
-			else:
-				ssh_installer()
-				continue
-		except:
-			print("Something went wrong... This script is for debian based distros..\n\n")
-			print("Checking placement of ssh keys...\n")
-			os.system("locate ssh_host_rsa_key")
-			break
